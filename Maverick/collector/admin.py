415ab3b19e29704ba48f52b3175d28040bc2bdcc
Zulou <rmil.cb@gmail.com>
2018-07-19 17:09:47 -0500

    asd

--- Maverick/collector/admin.py
+++ Maverick/collector/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-class ClientsAdmin(admin.ModelAdmin):
-    search_fields = ['id','firstname','lastname','dni','cellphone','email']
-    list_display = ('id','firstname','lastname','dni','cellphone','email')
 
-admin.site.register(models.clients,ClientsAdmin)
